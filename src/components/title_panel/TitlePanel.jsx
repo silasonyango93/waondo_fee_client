d10da9ac7e3c639e990ac4f63d8caedf16448c38
Silas Onyango <silas.onyango93@gmail.com>
2020-06-13 21:27:28 +0300

    complete title panel and topbar ui modification

--- src/components/title_panel/TitlePanel.jsx
+++ src/components/title_panel/TitlePanel.jsx
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
 
+import PropTypes from "prop-types";
 import './TitlePanel.scss';
 
 class TitlePanel extends Component {
     render() {
+
+        const {
+            title,
+            userName,
+            userEmail,
+            userNameInitials
+        } = this.props;
+
         return (
             <div className="panel__main-body">
                 <div className="panel__text-div">
                     <div className="col-sm-8 panel__left-column">
                         <div className="panel__inner-left-column">
-                            <div className="panel__title">Leave and holiday management</div>
+                            <div className="panel__title">{title}</div>
                             <div className="">{new Date().toDateString()+"  |  "+new Date().getHours()+":"+new Date().getMinutes()}</div>
                         </div>
 
@@ -19,16 +28,16 @@ class TitlePanel extends Component {
                             <div className="panel__profile-div">
                                 <div className="col-sm-8 panel__contact-div">
                                     <div className="panel__name-text">
-                                        Silas Onyango
+                                        {userName}
                                     </div>
                                     <div className="panel__email-text">
-                                        silas.onyango93@gmail.com
+                                        {userEmail}
                                     </div>
                                 </div>
                                 <div className="col-sm-4 panel__inner-profile-div">
                                     <div className="panel__profile-circle">
                                         <div className="panel__profile-circle-text">
-                                            SO
+                                            {userNameInitials}
                                         </div>
                                     </div>
                                 </div>
@@ -42,5 +51,19 @@ class TitlePanel extends Component {
         );
     }
 }
+
+TitlePanel.propTypes = {
+    title: PropTypes.string,
+    userName: PropTypes.string,
+    userEmail: PropTypes.string,
+    userNameInitials: PropTypes.string,
+};
+
+TitlePanel.defaultProps = {
+    title: 'Leave and holiday management',
+    userName: 'Silas Onyango',
+    userEmail: 'silas.onyango93@gmail.com',
+    userNameInitials: 'SO'
+};
 
 export default TitlePanel;
