a55c0cdb4b74f2d2f286988c30e7b3907d58ebbc
Silas Onyango <silas.onyango93@gmail.com>
2020-06-13 19:09:47 +0300

    title panel - ui progress

--- src/components/title_panel/TitlePanel.jsx
+++ src/components/title_panel/TitlePanel.jsx
@@ -7,13 +7,20 @@ class TitlePanel extends Component {
         return (
             <div className="panel__main-body">
                 <div className="panel__text-div">
-                    <div className="col-sm-8">
-                        <div className="panel__title">Leave and holiday management</div>
-                        <div className="">{new Date().toDateString()+"  |  "+new Date().getHours()+":"+new Date().getMinutes()}</div>
+                    <div className="col-sm-8 panel__left-column">
+                        <div className="panel__inner-left-column">
+                            <div className="panel__title">Leave and holiday management</div>
+                            <div className="">{new Date().toDateString()+"  |  "+new Date().getHours()+":"+new Date().getMinutes()}</div>
+                        </div>
+
                     </div>
                     <div className="col-sm-4 panel__right-column">
                         <div className="panel__profile-div">
-                            <div className="col-sm-6">y</div>
+                            <div className="col-sm-6 panel__contact-div">
+                                <div className="panel__contact-inner-div">
+                                    y
+                                </div>
+                            </div>
                             <div className="col-sm-6 panel__inner-profile-div">
                                 <div className="panel__profile-circle">
                                     <div className="panel__profile-circle-text">
