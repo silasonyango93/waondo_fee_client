f6c53598d7d8dd5a9c771a53f35f5e853233021f
Silas Onyango <silas.onyango93@gmail.com>
2020-06-13 22:57:43 +0300

    working html2canvas

--- src/views/staff_home/student_management/students/StudentsPage.jsx
+++ src/views/staff_home/student_management/students/StudentsPage.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 import Table from "../../../../components/table/table_body/Table";
+import html2canvas from 'html2canvas'
 
 class StudentsPage extends Component {
+
+    printDiv = () =>{
+        html2canvas(document.getElementById("main"), {scale: 8}).then(canvas => {
+            var link = document.createElement('a');
+            link.download = 'receipt.png';
+            link.href = canvas.toDataURL()
+            link.click();
+        });
+    };
     render() {
         return (
-            <div>
+            <div id="main">
                 <Table />
             </div>
         );
